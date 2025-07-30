fa3a79e37e3bec2a9b6e2ca914a096b80c2040c2
Shoilee <sarahshoilee121@gmail.com>
2025-07-30 17:40:36 +0200

    update modularity

--- main2.py
+++ main2.py
@@ -3,10 +3,10 @@ import os, json
 from LLM import call_LLM
 from LLM_key import llm_model
 from parse import extract_HTML, format_td
-from prompt import tsr_html_prompt
+from prompt import tsr_html_prompt, tsr_html_decomposition
 from metric import TEDS
 
-def save_response_to_file(output_path, response):
+def save_response_to_file(output_path, image_name, response):
     html_response_path = os.path.join(output_path, os.path.splitext(image_name)[0]+".html")
     html_response = response
     
@@ -15,7 +15,7 @@ def save_response_to_file(output_path, response):
     return  
 
 
-if __name__ == "__main__":
+def main(strategy : str, temp = 0):
     teds = TEDS(structure_only=False)
     teds_struct = TEDS(structure_only=True)
 
@@ -24,7 +24,10 @@ if __name__ == "__main__":
     # label_html = item["html"]
     # difficulty = item["type"]
 
-    llm_html = call_LLM(image_path, prompt=tsr_html_prompt, model_name=llm_model, temperature=0)
+    if strategy == "chain_of_thought":
+        llm_html = call_LLM(image_path, prompt=tsr_html_decomposition, model_name=llm_model, temperature=temp)
+    else:
+        llm_html = call_LLM(image_path, prompt=tsr_html_prompt, model_name=llm_model, temperature=temp)
     llm_html = extract_HTML(llm_html)
 
     llm_html = format_td(llm_html)
@@ -45,12 +48,15 @@ if __name__ == "__main__":
             "llm_prompt": llm_html,
             "label": label_html,
         }
-    output_path = f"output/{llm_model.split('/')[-1]}"
-    output_name = "/" + "llm_output" + ".jsonl"
+    output_path = f"output/{strategy}/{llm_model.split('/')[-1]}"
+    output_name = "/" + strategy + ".jsonl"
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     with open(output_path + output_name, 'a', encoding='utf-8') as f:
         f.write(json.dumps(output_json) + "\n")
 
-    save_response_to_file(output_path, llm_html)
+    save_response_to_file(output_path, image_name, llm_html)
+
+if __name__ == "__main__":
+    main("chain_of_thought")
